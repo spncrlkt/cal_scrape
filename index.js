36e62cea5728220d500646193a5d6a7d2d330614
sliechty <spencerliechty@gmail.com>
2016-02-16 21:35:44 -0800

    OAuth example running

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
-var google = require('googleapis');
 
+var google = require('googleapis');
 var readline = require('readline');
+
+var secrets = require('./secrets.json');
+
+var OAuth2Client = google.auth.OAuth2;
+var plus = google.plus('v1');
+
+// Client ID and client secret are available at
+// https://code.google.com/apis/console
+var CLIENT_ID = secrets.installed.client_id;
+var CLIENT_SECRET = secrets.installed.client_secret;
+var REDIRECT_URL = secrets.installed.redirect_uris[0];
+
+// var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+var oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, 'oob');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function getAccessToken(oauth2Client, callback) {
+  // generate consent page url
+  var url = oauth2Client.generateAuthUrl({
+    access_type: 'offline', // will return a refresh token
+    scope: 'https://www.googleapis.com/auth/plus.me' // can be a space-delimited string or an array of scopes
+  });
+
+  console.log('Visit the url: ', url);
+  rl.question('Enter the code here:', function(code) {
+    // request access token
+    oauth2Client.getToken(code, function(err, tokens) {
+      // set tokens to the client
+      // TODO: tokens should be set by OAuth2 client.
+      oauth2Client.setCredentials(tokens);
+      callback();
+    });
+  });
+}
+
+// retrieve an access token
+getAccessToken(oauth2Client, function() {
+  // retrieve user profile
+  plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, profile) {
+    if (err) {
+      console.log('An error occured', err);
+      return;
+    }
+    console.log(profile.displayName, ':', profile.tagline);
+  });
+});
